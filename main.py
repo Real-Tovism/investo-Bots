29fd49f1566b342867829411d6d6ff41782101fc
Rezolotion <74096956+Real-Tovism@users.noreply.github.com>
2025-01-30 13:16:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+# تنظیم دسترسی‌ها
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+
+# خواندن اطلاعات سرویس‌اکانت از فایل JSON
+creds = ServiceAccountCredentials.from_json_keyfile_name("serene-broker-449408-a4-44be75670742.json", scope)
+client = gspread.authorize(creds)
+
+# باز کردن شیت مورد نظر
+spreadsheet = client.open("InvestoBots Data")  # نام فایل اکسل که توی گوگل شیت ساختی
+worksheet = spreadsheet.sheet1  # اولین شیت رو انتخاب می‌کنه
+
+# دریافت داده‌ها
+data = worksheet.get_all_records()
+print(data)
